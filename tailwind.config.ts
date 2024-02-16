19c624d04fe830e16f0e7fb5aecc086b8375955b
Mardel J. Juma <mardel.mjj@gmail.com>
2024-02-16 12:12:12 +0200

    chore: :wrench: Setting up color pallet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "main": "#201C39",
+        "second": "#4ED4C5",
+        "white-mf": "#E4E5E7",
+        "least": "#17253D",
+        "blue-mf": "#191737",
+        "dark": "#121214",
+        "second-variation": "#15F8B6",
+        "purple-mf": "#15F8B6",
+        "orange-mf": "#FF9F24",
+        "pink-mf": "#FF5FCC"
+      }
     },
   },
   plugins: [],
