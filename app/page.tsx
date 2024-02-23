5a795c4d498a8efd16e81d4e20f0593faff90e14
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-24 00:51:23 +0200

    feat: add purpose section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from '@/components/Hero/Hero'; 
+import Hero from "@/components/Hero/Hero";
+import { Purpose } from "@/components/purpose/Purpose";
 
 export default function Home() {
   return (
-    <main className="h-full flex-col justify-center py-5 items-center ">
-
-     <Hero/>
-
+    <main className="h-full flex-col justify-center py-5 items-center antialiased">
+      <Hero />
+      <Purpose />
     </main>
   );
 }
