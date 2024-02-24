8b19a366fce7ab99a9fa21d006c2358d45b63eda
Jeffer Marcelino <jeffersunde72@gmail.com>
2024-02-24 22:46:35 +0200

    add info section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero/Hero'; 
+import { InformationSection } from '@/components/Information';
 
 export default function Home() {
   return (
     <main className="h-full flex-col justify-center py-5 items-center ">
 
      <Hero/>
+     <InformationSection />
 
     </main>
   );
