11f13ea03ff538a1f5c312393eaa5d1c4a52655f
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-26 03:30:59 +0200

    style: remove padding y

--- components/Information/Sponsor.tsx
+++ components/Information/Sponsor.tsx
@@ -1,21 +1,17 @@
-import { sponsorsLogos } from "@/data/mainData"
+import { sponsorsLogos } from "@/data/mainData";
 
 export const Sponsor = () => {
   return (
-    <section className="container w-full py-12">
-      <h2 className="uppercase text-white text-center">Parceirias e patrocínios</h2>
-
+    <section className="container w-full">
       <div className="w-full flex flex-wrap justify-center items-center gap-x-10 gap-y-2">
-        {
-          sponsorsLogos.map((image, index) => {
-            return (
-              <div key={ image.id }>
-                <img src={ image.src } alt="Sponsor image" />
-              </div>
-            )
-          })
-        }
+        {sponsorsLogos.map((image, index) => {
+          return (
+            <div key={image.id}>
+              <img src={image.src} alt="Sponsor image" />
+            </div>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
