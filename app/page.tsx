e2f169728813d0f5f22cba6d7cd604a98a3d1818
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-26 03:29:49 +0200

    feat: add cfp component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import { Event } from "@/components/Events/event";
 import Hero from "@/components/Hero/Hero";
-import { InformationSection } from '@/components/Information';  
+import { InformationSection } from "@/components/Information";
+import { CFP } from "@/components/cfp/CFP";
 import { Purpose } from "@/components/purpose/Purpose";
+import { Meteors } from "@/components/ui/meteoris";
 
 export default function Home() {
   return (
-    <main className="h-full flex-col justify-center py-5 items-center antialiased">
+    <main className="h-full flex flex-col gap-8 justify-center py-5 items-center">
       <Hero />
       <InformationSection />
       <Purpose />
       <Event />
+      <CFP />
     </main>
   );
 }
--- components/cfp/CFP.tsx
+++ components/cfp/CFP.tsx
@@ -1,26 +1,29 @@
-import { ArrowUpRight } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export const CFP = () => {
-    return (
-        <div className='bg-gradient-to-r from-teal-300 to-teal-700 rounded-lg container py-16 px-24'>
-            <div className="flex justify-between">
-                <div className="flex gap-4 items-center basis-3/5">
-                    <span className=" rounded-full bg-white p-10" />
+  return (
+    <section className="  bg-gradient-to-r from-teal-300 to-teal-700 rounded-lg container py-16 px-24">
+      <div className="flex justify-between">
+        <div className="flex gap-4 items-center basis-3/5">
+          <span className=" rounded-full bg-white p-10" />
 
-
-                    <h2 className=' font-bold text-3xl text-left text-wrap text-white'>Tem um tópico que gostava de  partilhar com a comunidade?</h2>
-                </div>
-
-                <div className=" font-semibold text-lg uppercase text-center items-center">
-                    <Link href="#" className="flex flex-row items-center rounded-lg w-full p-2 gap-2 bg-mf-secondary hover:bg-mf-secondaryVariation focus:ring-4  text-sm">
-                        Join Community
-                        <ArrowUpRight className='' />
-                    </Link>
-                </div>
-
-            </div>
+          <h2 className=" font-bold text-3xl text-left text-wrap text-white">
+            Tem um tópico que gostava de partilhar com a comunidade?
+          </h2>
         </div>
-    )
-}
+
+        <div className=" font-semibold text-lg uppercase text-center items-center">
+          <Link
+            href="#"
+            className="flex flex-row items-center rounded-lg w-full p-2 gap-2 bg-mf-secondary hover:bg-mf-secondaryVariation focus:ring-4  text-sm"
+          >
+            Join Community
+            <ArrowUpRight className="" />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
