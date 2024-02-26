e64ece041ad9f43771d02ce43dd161a97d04cef8
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-26 03:29:15 +0200

    feat: add floating navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 import { Navbar } from "@/components/navbar/Navbar";
 import { Footer } from "@/components/footer/Footer";
 import { cn } from "@/lib/utils";
+import { FloatingNav } from "@/components/navbar/floating-navbar";
 
-
-const mont = Montserrat({ subsets: ["latin"] })
+const mont = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Maputo Frontenders - Comunidade Frontend de Moçambique",
@@ -20,19 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={
-        cn(`bg-mf-primary`, mont.className)
-      }>
-        <div className='flex flex-col h-full'>
-
-          <Navbar />
-          <main className='h-full'>
-            {children}
-          </main>
+      <body className={cn(`bg-mf-background antialiased`, mont.className)}>
+        <div className="flex flex-col h-full">
+          <Navbar className="py-8" />
+          <FloatingNav />
+          <main className="h-full">{children}</main>
           <Footer />
-
         </div>
-
       </body>
     </html>
   );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,28 +1,37 @@
-import Link from 'next/link'
-import React from 'react'
-import { NavData, NavItem } from './Itens'
-import { ArrowUpRight } from 'lucide-react'
+import Link from "next/link";
+import React from "react";
+import { NavData, NavItem } from "./Itens";
+import { ArrowUpRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-export const Navbar = () => {
-    return (
-        <div className="container py-8 ">
-            <div className="bg-white bg-opacity-20 p-4 rounded-lg backdrop-blur-md  w-full mx-auto">
-                <div className='flex flex-row justify-between items-center'>
-                    <div className="text-white font-semibold text-lg">Logo</div>
-                    <div className="space-x-10">
-                        {NavData.map((item: NavItem, key: number) => (
-                            <Link key={key} href={item.label} className="text-white uppercase hover:text-mf-secondaryVariation">{item.label}</Link>
-                        ))}
-                    </div>
-                    <div className="text-white font-semibold text-lg uppercase text-center">
-                        <Link href="#" className="flex flex-row items-center text-white rounded-lg p-2 bg-mf-secondary hover:bg-mf-secondaryVariation focus:ring-4  text-sm">
-                            Join Community
-                            <ArrowUpRight className='text-white' />
-                        </Link>
-                    </div>
-                </div>
-            </div>
+// TODO: hover color on link button
+
+export const Navbar = ({ className }: { className?: string }) => {
+  return (
+    <div className={cn("container", className)}>
+      <div className="bg-mf-least p-4 rounded-lg backdrop-blur-md  w-full mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="w-52 text-white font-semibold text-lg">Logo</div>
+          <div className="space-x-10">
+            {NavData.map((item: NavItem, key: number) => (
+              <Link
+                key={key}
+                href={item.label}
+                className="text-white font-medium uppercase hover:text-mf-secondaryVariation"
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+          <Link
+            href="#"
+            className="w-52 flex items-center font-semibold uppercase text-center rounded-lg px-5 py-3 bg-mf-secondProposal hover:bg-mf-secondaryVariation  text-sm text-mf-least group"
+          >
+            Join Community
+            <ArrowUpRight className="ml-2 group-hover:" />
+          </Link>
         </div>
-
-    )
-}
+      </div>
+    </div>
+  );
+};
