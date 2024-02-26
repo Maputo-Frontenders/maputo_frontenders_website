73698f428bb5b35d1134826281d58e6f7df17b0c
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-26 03:28:18 +0200

    feat: update impact cards

--- components/Information/Impact.tsx
+++ components/Information/Impact.tsx
@@ -1,46 +1,39 @@
-import { impactCards } from "@/data/mainData"
-import { ArrowUpRight } from "lucide-react"
-import Link from "next/link"
+import { impactCards } from "@/data/mainData";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
 
 export const Impact = () => {
   return (
-    <section className="container w-full py-12 text-white flex flex-col items-center gap-6">
+    <section className="container w-full  text-white flex flex-col items-center gap-6">
       <div className="text-center">
         <h2 className="uppercase mb-3">Impacto</h2>
-        <h3 className="text-2xl font-bold mb-6">Contribuimos para o crescimento do ecossistema tecnológico local</h3>
-        <p className="max-w-3xl w-full mx-auto font-thin">Aprender, ensinar e descobrir tudo ligado ao desenvolvimento frontend, seja através de meetups, code challenges, hacktons e diversas outras formas de partilha de conhecimento e troca de experiencia.</p>
+        <h3 className="text-2xl font-bold mb-6">
+          Contribuimos para o crescimento do ecossistema tecnológico local
+        </h3>
+        <p className="max-w-3xl w-full mx-auto font-light">
+          Aprender, ensinar e descobrir tudo ligado ao desenvolvimento frontend,
+          seja através de meetups, code challenges, hacktons e diversas outras
+          formas de partilha de conhecimento e troca de experiencia.
+        </p>
       </div>
-
-      <Link href={"/"} className="py-4 px-6 rounded-lg flex gap-2 w-fit font-semibold uppercase" style={{
-        background: "linear-gradient(144.07deg, #4ED4C5 -53.58%, #A679FF 120.88%),linear-gradient(0deg, #17253D, #17253D);"
-      }}>
-        Join community
-        <ArrowUpRight className='text-white h-6 w-5' />
-      </Link>
-
-      <div className="mt-16 flex flex-wrap justify-center gap gap-6">
-        {
-          impactCards.map((card) => {
-            return (
-              <div className="bg-mf-least pt-16 px-14 rounded-lg overflow-hidden h-fit" key={ card.title }>
-                <div className={`flex flex-col gap-1 text-center ${ !card.button && "pb-16" }`}>
-                  <span className="text-gradient text-5xl font-bold">{ card.number }+</span>
-                  <span className="text-lg">{ card.title }</span>
-                </div>
-
-                {
-                  card.button && (
-                    <Link href={card.button.href} className="mt-12 mb-9 bg-mf-blue py-4 px-6 rounded-lg flex gap-2 w-fit font-semibold uppercase">
-                      { card.button.title }
-                      <ArrowUpRight className='text-white h-6 w-5' />
-                    </Link>
-                  )
-                }
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap gap-6">
+        {impactCards.map((card) => {
+          return (
+            <div
+              className="bg-mf-least pt-16 px-8 overflow-hidden col-span-1 h-full rounded-lg"
+              key={card.title}
+            >
+              <div className={`flex flex-col gap-2 text-center ${"pb-16"}`}>
+                <span className="text-gradient text-5xl font-bold">
+                  {card.number}
+                </span>
+                <span className="text-lg font-bold">{card.title}</span>
+                <span className=" text-left">{card.description}</span>
               </div>
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
--- data/mainData.ts
+++ data/mainData.ts
@@ -1,66 +1,64 @@
 const teamMembers = [
-    {
-        name: "Ancha", role: "Presidente", photo: "url"
-    },
-    {
-        name: "Ancha", role: "Presidente", photo: "url"
-    }
+  {
+    name: "Ancha",
+    role: "Presidente",
+    photo: "url",
+  },
+  {
+    name: "Ancha",
+    role: "Presidente",
+    photo: "url",
+  },
 ];
 
 export const sponsorsLogos = [
-    {
-        id: 1,
-        src: "/sponsors/mozdevz.png",
-    },
-    {
-        id: 2,
-        src: "/sponsors/kabum.png",
-    },
-    {
-        id: 3,
-        src: "/sponsors/mozdevz.png",
-    },
-    {
-        id: 4,
-        src: "/sponsors/kabum.png",
-    },
-    {
-        id: 5,
-        src: "/sponsors/mozdevz.png",
-    },
-    {
-        id: 6,
-        src: "/sponsors/kabum.png",
-    },
-    {
-        id: 7,
-        src: "/sponsors/mozdevz.png",
-    },
+  {
+    id: 1,
+    src: "/sponsors/mozdevz.png",
+  },
+  {
+    id: 2,
+    src: "/sponsors/kabum.png",
+  },
+  {
+    id: 3,
+    src: "/sponsors/mozdevz.png",
+  },
+  {
+    id: 4,
+    src: "/sponsors/kabum.png",
+  },
+  {
+    id: 5,
+    src: "/sponsors/mozdevz.png",
+  },
+  {
+    id: 6,
+    src: "/sponsors/kabum.png",
+  },
+  {
+    id: 7,
+    src: "/sponsors/mozdevz.png",
+  },
 ];
 
 export const impactCards = [
-    {
-        number: 500,
-        title: "Membros",
-    },
-    {
-        number: 26,
-        title: "Encontros",
-        button: {
-            title: "Past events",
-            href: "/"
-        }
-    },
-    {
-        number: 1000,
-        title: "Membros",
-    },
-    {
-        number: 26,
-        title: "Aqui",
-        button: {
-            title: "Subscribe",
-            href: "/"
-        }
-    }
-]
+  {
+    number: 500,
+    title: "Membros",
+    description:
+      "Esses encontros, realizados tanto de forma virtual quanto presencial, proporcionam um espaço valioso para  networking e partilha de conhecimento",
+  },
+  {
+    number: 26,
+    title: "Encontros",
+    description:
+      "Esses encontros, realizados tanto de forma virtual quanto presencial, proporcionam um espaço valioso para  networking e partilha de conhecimento",
+  },
+  {
+    number: 1000,
+    title: "Membros",
+    description:
+      "A interação e o engajamento não se limitam apenas aos eventos formais, e nosso grupo no WhatsApp é a prova disso. ",
+  },
+];
