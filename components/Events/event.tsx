b532a5a2619a46559808786ad179a5b46bf21f06
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-26 03:30:08 +0200

    style: increase padding

--- components/Events/event.tsx
+++ components/Events/event.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export const Event = () => {
   return (
-    <section className="bg-mf-dark text-mf-white space-y-8 py-10 flex justify-center items-center flex-col">
+    <section className="w-full bg-mf-dark text-mf-white space-y-8 py-10 flex justify-center items-center flex-col">
       <h4 className="uppercase text-sm text-center">Eventos</h4>
       <h2 className="text-2xl font-bold text-center max-w-3xl">
         Junte-se a nós e participe dos eventos que estão moldando o futuro do
@@ -19,10 +19,13 @@ export const Event = () => {
 
       <div className="flex justify-between gap-4">
         {events.map((item) => (
-          <div className="bg-mf-least rounded-md max-w-80 overflow-hidden flex flex-col gap-4">
+          <div
+            className="bg-mf-least rounded-md max-w-80 overflow-hidden flex flex-col gap-4"
+            key={item.urlImage}
+          >
             <Image src={CodeImage} alt="" className="w-full" />
 
-            <div className="px-4 flex flex-col gap-2">
+            <div className="px-4 pb-7 flex flex-col gap-2">
               <span className=" w-min rounded-2xl bg-[#262B42] p-2 px-4 text-mf-secondary text-xs font-bold">
                 {item.eventType}
               </span>
