f354d99b12fb02497d85257db25c96f34f656fdd
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-27 02:31:04 +0200

    feat: add teammember section

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import Hero from "@/components/Hero/Hero";
 import { InformationSection } from "@/components/Information";
 import { CFP } from "@/components/cfp/CFP";
 import { Purpose } from "@/components/purpose/Purpose";
-import { Meteors } from "@/components/ui/meteoris";
+import { TeamMembers } from "@/components/teammembers/TeamMembers";
 
 export default function Home() {
   return (
-    <main className="h-full flex flex-col gap-8 justify-center py-5 items-center">
+    <main className="h-full flex flex-col gap-12 justify-center py-5 items-center">
       <Hero />
       <InformationSection />
       <Purpose />
       <Event />
+      <TeamMembers />
       <CFP />
     </main>
   );
