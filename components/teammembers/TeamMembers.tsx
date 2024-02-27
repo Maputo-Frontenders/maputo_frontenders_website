bc81187641be6dee5e99764ab934b697cefffa75
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-27 02:30:50 +0200

    feat: add blur background

--- components/teammembers/TeamMembers.tsx
+++ components/teammembers/TeamMembers.tsx
@@ -1,38 +1,32 @@
-import React from 'react'
-import { team } from './Items'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
-import Image from 'next/image'
-import img from "../../public/assets/images/Ancha Pedro.png"
-import { TeamMember } from '../cards/TeamMember'
+import React from "react";
+import { team } from "./Items";
+import Image from "next/image";
+import { TeamMember } from "../cards/TeamMember";
+import BlurBackground from "@/public/blur-background.svg";
+
 export const TeamMembers = () => {
-    return (
-        <div className='text-center gap-4 flex flex-col'>
-            <div className="flex flex-col ga-2">
-                <h5 className=' font-normal text-sm text-white'>Nossa team</h5>
-                <h2 className='text-white font-bold text-4xl'>Conheça a equipe por detrás  da Maputo Frontenders</h2>
-
-            </div>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
-                {/* <div className="w-full max-w-[500px] sm:max-w-[300px]">
-                    <div className="relative group mb-5">
-                        <div>
-                            <Image src={img} alt={""} className="w-full" />
-                        </div>
-                        <div className="bg-gradient-to-b from-transparent via-transparent to-black py-5 absolute bottom-0 w-full opacity-500 transition-opacity duration-700">
-                            <div className="flex gap-10 justify-center text-white">
-                                prrrrr
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-
-                {team.map((item) =>
-                    <div key={item.name}>
-
-                        <TeamMember {...item} />
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="text-center gap-4 flex flex-col">
+      <div className="flex flex-col ga-2">
+        <h4 className=" font-normal text-sm text-white uppercase">
+          Nossa team
+        </h4>
+        <h2 className="text-white font-bold text-2xl">
+          Conheça a equipe por detrás da Maputo Frontenders
+        </h2>
+      </div>
+      <div className="relative grid grid-cols-2 md:grid-cols-4 gap-4 overflow-hidden">
+        <Image
+          src={BlurBackground}
+          alt=""
+          className="absolute -z-50 top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+        {team.map((item) => (
+          <div key={item.name}>
+            <TeamMember {...item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
