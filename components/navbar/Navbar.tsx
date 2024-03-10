ed8699acd7668486275a3fed0385460708285766
raymolide <raymolide@gmail.com>
2024-03-10 13:06:59 +0200

    fix: navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -18,10 +18,10 @@ export const Navbar = ({ className }: { className?: string }) => {
   return (
     <div className={cn("container", className)}>
       <nav className="bg-mf-least rounded-lg backdrop-blur-md w-full mx-auto">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <div className="flex items-center space-x-5  ">
+        <div className="w-full flex flex-wrap items-center justify-between  p-4">
+          <div className="flex items-center space-x-5">
             <button onClick={toggleMenu} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center px-2 py-3  justify-center text-sm text-white rounded-lg md:hidden hover:bg-mf-secondProposalHover focus:outline-none focus:ring-2 focus:ring-mf-secondProposal dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
+             
 
               {!isMenuOpen ? <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -30,23 +30,23 @@ export const Navbar = ({ className }: { className?: string }) => {
               </svg>}
 
             </button>
-            <Image width={48} height={48} src={Logo} alt="Maputo Frontenders" />
+            <Image className="max-[400px]:hidden" width={48} height={48} src={Logo} alt="Maputo Frontenders" />
           </div>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 ">
+          <div className="flex md:order-2 space-x-2 md:space-x-0 ">
             <Link
               href="#"
               className="w-full truncate text-clip  flex items-center justify-center font-semibold uppercase text-center rounded-lg px-5 py-3 bg-mf-secondProposal hover:bg-mf-secondProposalHover text-sm text-mf-least group"
             >
               Junte-se a nós
-              <ArrowUpRight className="ml-2 group-hover:-translate-y-1 hover:scale-110 duration-300" />
+              <ArrowUpRight className="max-[400px]:hidden ml-2 group-hover:-translate-y-1 hover:scale-110 duration-300" />
             </Link>
 
           </div>
-          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1  ${!isMenuOpen && "sm:hidden"}`} id="navbar-cta">
-            <div className="flex flex-col font-medium p-4 md:p-0 mt-4 border  rounded-lg md:space-x-8  md:flex-row md:mt-0 md:border-0   dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              {NavData.map((item: NavItem, key: number) => (
+          <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1  ${!isMenuOpen && "sm:flex"}`} id="navbar-cta">
+            <div className={`flex-col font-medium p-4 md:p-0 mt-4 border  rounded-lg md:space-x-8  md:flex-row md:mt-0 md:border-0 md:flex lg:flex ${!isMenuOpen ? "hidden" : "flex"} dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}>
+              {NavData.map((item: NavItem) => (
                 <Link
-                  key={key}
+                  key={item.label}
                   href={item.link}
                   className="block py-2 px-3 md:p-0 text-white font-medium uppercase hover:text-mf-secondaryVariation"
                 >
