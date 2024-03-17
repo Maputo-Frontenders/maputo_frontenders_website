7c86c3342be75936694c85c2607b173290923e36
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-03-17 16:38:48 +0200

    style: add mf system design

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,23 +1,25 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-mf-secondProposal text-mf-primary hover:bg-mf-secondProposal/90 truncate text-clip  flex items-center justify-center text-center rounded-lg px-5 py-3 group",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-mf-secondProposal text-mf-secondProposal bg-transparent hover:bg-mf-secondProposal/10 truncate text-clip  flex items-center justify-center rounded-lg px-5 py-3 group",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 truncate text-clip  flex items-center justify-center text-center rounded-lg px-5 py-3 group",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground truncate text-clip  flex items-center justify-center text-center rounded-lg px-5 py-3 group",
+        link: "text-mf-secondProposal underline-offset-4 hover:underline truncate text-clip  flex items-center justify-center text-center rounded-lg px-5 py-3 group",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -31,26 +33,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
