8640008e114c329b1e681adfeae331166d57698a
Mardel J. Juma <mardel.mjj@gmail.com>
2024-03-17 12:44:06 +0200

    feat: LoadingUI
    Tried to implement Suspense API!

--- app/eventos/page.tsx
+++ app/eventos/page.tsx
@@ -5,7 +5,8 @@ import { Button } from '@/components/ui/button';
 import { fetchData } from '@/data/fetchEventData';
 import useGetEvents from '@/hooks/useGetEvents';
 import { FetchDataResponse } from '@/models/fetchDataResponse';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
+import Loading from './loading';
 
 export default function Eventos() {
     const { events, fetchMore } = useGetEvents();
@@ -17,7 +18,10 @@ export default function Eventos() {
                 <h5 className=' font-normal text-sm'>EVENTOS</h5>
                 <h1 className=' font-bold text-2xl'>Junte-se a nós  e participe dos eventos que estão moldando o futuro do desenvolvimento frontend em Moçambique</h1>
             </div>
-            <EventList eventos={events?.results} />
+            <Suspense fallback={<Loading />}>
+
+                <EventList eventos={events?.results} />
+            </Suspense>
             <Button onClick={() => { setGetMore(getMore + 1); fetchMore(); }} className='truncate text-clip  flex items-center justify-center font-semibold uppercase text-center rounded-lg px-5 py-3 bg-mf-secondProposal hover:bg-mf-secondProposalHover text-sm text-mf-least group'>
                 Carregar mais
             </Button>
