667c9343c7c17d4093696c5aea24dc17611741ed
Mardel J. Juma <mardel.mjj@gmail.com>
2024-04-08 17:47:20 +0200

    feat: loading events cards

--- app/eventos/page.tsx
+++ app/eventos/page.tsx
@@ -18,10 +18,9 @@ export default function Eventos() {
                 <h5 className=' font-normal text-sm'>EVENTOS</h5>
                 <h1 className=' font-bold text-2xl'>Junte-se a nós  e participe dos eventos que estão moldando o futuro do desenvolvimento frontend em Moçambique</h1>
             </div>
-            <Suspense fallback={<Loading />}>
 
-                <EventList eventos={events?.results} />
-            </Suspense>
+            <EventList eventos={events?.results} />
+
             <Button onClick={() => { setGetMore(getMore + 1); fetchMore(); }} className='truncate text-clip  flex items-center justify-center font-semibold uppercase text-center rounded-lg px-5 py-3 bg-mf-secondProposal hover:bg-mf-secondProposalHover text-sm text-mf-least group'>
                 Carregar mais
             </Button>
--- components/Events/EventList.tsx
+++ components/Events/EventList.tsx
@@ -1,15 +1,18 @@
 import { Evento } from '@/models/evento'
 import React from 'react'
 import { EventCard } from '../cards/EventCard'
+import LoadingCards from '../cards/LoadingCards'
 
 export const EventList = (eventos: any) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-            {eventos?.eventos?.map((evento: Evento) =>
-                <div key={evento.title}>
+            {eventos.eventos === undefined ? Array(4).fill(<LoadingCards />) :
+                eventos.eventos?.map((evento: Evento) =>
+                    <div key={evento.title}>
 
-                    <EventCard {...evento} />
-                </div>)}
+                        <EventCard {...evento} />
+                    </div>)}
+            { }
         </div>
     )
 }
