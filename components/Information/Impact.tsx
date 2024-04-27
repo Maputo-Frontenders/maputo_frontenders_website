0378ddb03f2d209ae5021dbb3fd0ea11d803182c
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-04-28 00:34:40 +0200

    fix: height issue

--- components/Information/Impact.tsx
+++ components/Information/Impact.tsx
@@ -2,7 +2,10 @@ import { impactCards } from "@/data/mainData";
 
 export const Impact = () => {
   return (
-    <section className="container w-full  text-white flex flex-col items-center gap-6">
+    <section
+      id="impacto"
+      className="container w-full text-white flex flex-col items-center gap-6"
+    >
       <div className="text-center">
         <h2 className="uppercase mb-3">Impacto</h2>
         <h3 className="text-2xl font-bold mb-6">
@@ -17,13 +20,13 @@ export const Impact = () => {
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap gap-6">
         {impactCards.map((card) => {
           return (
-            <div className="bg-mf-least pt-16 px-8 overflow-hidden col-span-1 h-full rounded-lg border-2 border-mf-white/5 hover:border-mf-white/10 duration-150">
-              <div className={`flex flex-col gap-2 text-center ${"pb-16"}`}>
+            <div className="bg-mf-least py-16 px-8 overflow-hidden col-span-1 rounded-lg border-2 border-mf-white/5 hover:border-mf-white/10 duration-150">
+              <div className={`flex flex-col gap-2 text-center`}>
                 <span className="text-gradient text-5xl font-bold">
                   +{card.number}
                 </span>
                 <span className="text-lg font-bold">{card.title}</span>
-                <span className=" text-left">{card.description}</span>
+                <span className=" text-center">{card.description}</span>
               </div>
             </div>
           );
