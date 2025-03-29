70c12248efcb3619188c855ada9b98100b035a5d
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2025-03-29 15:46:52 +0200

    fix: update card mapping to include localization

--- sections/stats/index.tsx
+++ sections/stats/index.tsx
@@ -33,7 +33,7 @@ export function ImpactSection({ intl }: Props) {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-10">
-          {cards.map((card) => (
+          {cards({ intl }).map((card) => (
             <ImpactCard
               icon={card.icon}
               title={card.title}
