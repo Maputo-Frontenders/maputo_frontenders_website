8f43916b6e7f0fbe9f00a08f9640dbab55bf5602
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2025-04-12 18:41:15 +0200

    feat: update testimonial and sponsor image paths for improved organization
    - refactored image paths in testimonials and sponsors data to use imported assets

--- utils/main-data.ts
+++ utils/main-data.ts
@@ -1,28 +1,34 @@
+import PropelLogo from "@/assets/sponsors/propel.png";
+import KabumLogo from "@/assets/sponsors/kabum.png";
+import BaobaHubLogo from "@/assets/sponsors/baoba-hub.png";
+import OnitLogo from "@/assets/sponsors/onit-logo.png";
+import MozDevzLogo from "@/assets/sponsors/mozdevz.png";
+
 export const sponsors_logos = [
   {
     id: 5,
     alt: "propel",
-    src: "/sponsors/propel.png",
+    src: PropelLogo,
   },
   {
     id: 2,
     alt: "kabum",
-    src: "/sponsors/kabum.png",
+    src: KabumLogo,
   },
   {
     id: 3,
     alt: "baoba-hub",
-    src: "/sponsors/baoba-hub.png",
+    src: BaobaHubLogo,
   },
   {
     id: 4,
     alt: "onit",
-    src: "/sponsors/onit-logo.png",
+    src: OnitLogo,
   },
 
   {
     id: 1,
     alt: "mozdevz",
-    src: "/sponsors/mozdevz.png",
+    src: MozDevzLogo,
   },
 ];
