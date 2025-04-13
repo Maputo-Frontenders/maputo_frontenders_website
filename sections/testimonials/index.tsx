2932e0a639878d501b586c59f1c347485abf4cad
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2025-04-13 13:30:09 +0200

    refactor: remove unused CTA link from testimonial section

--- sections/testimonials/index.tsx
+++ sections/testimonials/index.tsx
@@ -32,18 +32,6 @@ export function TestimonialSection({ intl }: Props) {
       </div>
 
       <TestimonialCarousel testimonials={testimonials} />
-
-      <Link
-        href={"#"}
-        className="w-fit mx-auto flex items-center justify-center font-semibold uppercase text-center rounded-lg px-5 py-3 bg-mf-secondProposal hover:bg-mf-secondProposalHover text-sm text-mf-least group"
-        aria-label={intl.hero.cta}
-      >
-        {intl.hero.cta}
-        <ArrowUpRight
-          className="max-[400px]:hidden ml-2 group-hover:-translate-y-1 group-hover:translate-x-1 duration-300"
-          aria-hidden="true"
-        />
-      </Link>
     </section>
   );
 }
